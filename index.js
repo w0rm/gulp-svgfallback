e909f1925dfa46d325ad46e369665d178d5a71f1
Andrey Kuzmin <unsoundscapes@gmail.com>
2015-01-03 22:24:48 +0100

    Prettify code with promises

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ var gutil = require('gulp-util')
 var _ = require('lodash')
 var phridge = require('phridge')
 var fs = require('fs')
+var when = require('when')
 
 
 module.exports = function (options) {
 
   options = options || {}
 
-  var spriteTemplate = options.spriteTemplate ||
-                       path.join(__dirname, 'templates', 'sprite.html')
-  var cssTemplate = options.cssTemplate ||
-                    path.join(__dirname, 'templates', 'style.css')
+  var spriteTemplate = options.spriteTemplate || path.join(__dirname, 'templates', 'sprite.html')
+  var cssTemplate = options.cssTemplate || path.join(__dirname, 'templates', 'style.css')
   var pngFileName = options.pngFileName || 'svgfallback.png'
   var cssFileName = options.cssFileName || 'svgfallback.css'
   var backgroundUrl = options.backgroundUrl || pngFileName
@@ -27,7 +26,7 @@ module.exports = function (options) {
         return cb(new gutil.PluginError('gulp-svgstore', 'Streams are not supported!'))
       }
       svgs.push({
-        content: file.contents.toString('utf8')
+        content: file.contents.toString()
       , name: path.basename(file.relative, path.extname(file.relative))
       })
       cb()
@@ -39,70 +38,76 @@ module.exports = function (options) {
 
       if (svgs.length === 0) return cb()
 
-      renderTemplate(spriteTemplate, {icons: svgs}, function (err, html) {
-        if (err) {
-          return cb(new gutil.PluginError('gulp-svgfallback', err))
-        }
-        phridge
-          .spawn()
-          .then(function (phantom) {
-            return phantom
-              .run(html, function (html, resolve) {
-                var icons
-                var rect
-                var page = webpage.create()
-                page.content = html
-                rect = page.evaluate(function () {
-                  return document.querySelector('.icons').getBoundingClientRect()
-                })
-                icons = page.evaluate(function () {
-                  var all = document.querySelectorAll('.icon')
-                  return [].map.call(all, function (el) {
-                    var result = el.getBoundingClientRect()
-                    result.name = el.getAttribute('data-name')
-                    return result
-                  })
-                })
-                page.clipRect = rect
-                resolve({
-                  img: page.renderBase64('PNG')
-                , icons: icons
-                })
-              })
-              .finally(phantom.dispose.bind(phantom))
+      renderTemplate(spriteTemplate, {icons: svgs})
+        .then(generateSprite)
+        .then(function (sprite) {
+
+          self.push(new gutil.File({
+            path: pngFileName
+          , contents: new Buffer(sprite.img, 'base64')
+          }))
+
+          return renderTemplate(cssTemplate, {
+            backgroundUrl: backgroundUrl
+          , icons: sprite.icons
+          , prefix: prefix
           })
-          .done(function (result) {
-            var pngFile = new gutil.File({
-              path: pngFileName
-            , contents: new Buffer(result.img, 'base64')
-            })
-            var cssOptions = {
-              backgroundUrl: backgroundUrl
-            , icons: result.icons
-            , prefix: prefix
-            }
-            self.push(pngFile)
-            renderTemplate(cssTemplate, cssOptions, function (err, css) {
-              if (err) {
-                return cb(new gutil.PluginError('gulp-svgfallback', err))
-              }
-              var cssFile = new gutil.File({
-                path: cssFileName
-              , contents: new Buffer(css)
-              })
-              self.push(cssFile)
-              cb()
-            })
-          }, function (err) {
-            cb(new gutil.PluginError('gulp-svgfallback', err))
-          })
-      })
+
+        })
+        .done(
+          function (css) {
+            self.push(new gutil.File({
+              path: cssFileName
+            , contents: new Buffer(css)
+            }))
+            cb()
+          }
+        , function (err) {
+            cb(new gutil.PluginError('gulp-svgstore', err))
+          }
+        )
     }
   )
 }
 
 
-function renderTemplate (template, data, cb) {
-  var template = fs.readFileSync(template, {encoding: 'utf8'})
-  cb(null, _.template(template, data))
+function renderTemplate (fileName, options) {
+  return when.promise(function (resolve, reject) {
+    fs.readFile(fileName, function (err, template) {
+      if (err) return reject(err)
+      try {
+        resolve(_.template(template, options))
+      } catch (err) {
+        reject(err)
+      }
+    })
+  })
+}
+
+
+function generateSprite (html) {
+  return phridge.spawn()
+    .then(function (phantom) {
+      return phantom
+        .run(html, function (html, resolve) {
+          var icons
+          var rect
+          var page = webpage.create()  // jshint ignore: line
+          page.content = html
+          rect = page.evaluate(function () {
+            return document.querySelector('.icons').getBoundingClientRect()
+          })
+          icons = page.evaluate(function () {
+            var all = document.querySelectorAll('.icon')
+            return [].map.call(all, function (el) {
+              var result = el.getBoundingClientRect()
+              result.name = el.getAttribute('data-name')
+              return result
+            })
+          })
+          page.clipRect = rect
+          resolve({ img: page.renderBase64('PNG'), icons: icons })
+        })
+        .finally(phantom.dispose.bind(phantom))
+    })
 }
